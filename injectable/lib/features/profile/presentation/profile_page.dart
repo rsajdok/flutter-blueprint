830ae15b072f25e246aa9e1d19d8f45d3001f4e8
Robert Sajdok <robert.sajdok@gmail.com>
2025-04-05 21:52:54 +0200

    add new project 'injectable'

---
+++ injectable/lib/features/profile/presentation/profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScreenB extends StatelessWidget {
+  const ScreenB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Congrats!'),),);
+  }
+}
